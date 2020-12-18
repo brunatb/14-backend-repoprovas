38b11354abe57fb4b7be5901e33cdbc3834954ac
brunatb <brutopanb@gmail.com>
2020-12-18 20:54:40 -0300

    Getting semesters and class name

--- src/repositories/classesRepository.js
+++ src/repositories/classesRepository.js
@@ -5,4 +5,23 @@ async function getAllClasses() {
     return classes.rows;
 }
 
-module.exports = { getAllClasses }
+async function getAllClassesInfo(semesterId){
+    const classes = await db.query(`SELECT *,
+    (SELECT count("classId") AS "numberOfTests" FROM tests WHERE tests."classId" = classes.id 
+    AND tests."semesterId" = $1)
+    from classes`, [semesterId]);
+
+    return classes.rows
+}
+
+async function getClassById(classId){
+    const className = await db.query(`SELECT name FROM classes WHERE id = $1`, [classId]);
+
+    return className.rows[0];
+}
+
+module.exports = { 
+    getAllClasses,
+    getAllClassesInfo, 
+    getClassById
+}
